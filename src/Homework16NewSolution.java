fe03b6a2733e05d84fb38837af33aca6dd8a39a0
meldasav <gmeldag83@hotmail.com>
2022-04-17 23:54:25 -0500

    Homework16bettersolution

--- src/Homework16NewSolution.java
+++ src/Homework16NewSolution.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 
 public class Homework16NewSolution {
     public static void main(String[] args) {
-        System.out.println(countPos(new ArrayList<>(Arrays.asList(-45, 0, 0, 34, 5, 67))));
+        System.out.println(removeDuplicateElements(new ArrayList<>(Arrays.asList("java", "C#", "ruby", "JAVA", "ruby", "C#", "C++"))));
+        System.out.println(removeExtraSpaces("     java    is   fun     "));
+        System.out.println(Arrays.toString(add(new int[]{3, 0, 0, 7, 5, 10}, new int[]{10, -13, 15, 12, 70, 5, 8, 57})));
     }
 
     /**
@@ -41,29 +43,94 @@ public class Homework16NewSolution {
     }
 
     /**
-     2nd WAY
+     * 2nd WAY
      */
-    public static int countA1(String str){
+    public static int countA1(String str) {
         int countA = 0;
-        for(char c : str.toCharArray()){
-            if(c == 'A' || c == 'a') countA++;
+        for (char c : str.toCharArray()) {
+            if (c == 'A' || c == 'a') countA++;
         }
         return countA;
     }
+
     /**
      * Write a method countPos() that takes an ArrayList of Integer as an argument,
-       and returns how many elements are positive
+     * and returns how many elements are positive
      */
-    public static int countPos(ArrayList<Integer> numbers){
-        int countPos=0;
+    public static int countPos(ArrayList<Integer> numbers) {
+        int countPos = 0;
         for (Integer number : numbers) {
-             if(number>0)countPos++;
+            if (number > 0) countPos++;
         }
         return countPos;
     }
 
+    /**
+     * Write a method removeDuplicateNumbers() that takes an ArrayList of Integer as an argument,
+     * and returns it back with removed duplicates
+     */
 
+    public static ArrayList<Integer> removeDuplicateNumbers(ArrayList<Integer> numbers) {
+        ArrayList<Integer> number = new ArrayList<>();
+        for (Integer integer : numbers) {
+            if (!number.contains(integer)) number.add(integer);
+        }
+        return number;
+    }
 
+    /**
+     * Write a method removeDuplicateElements() that takes an ArrayList of String as an argument,
+     * and returns it back with removed duplicates
+     */
 
+    public static ArrayList<String> removeDuplicateElements(ArrayList<String> str) {
+        ArrayList<String> str1 = new ArrayList<>();
+        String s1 = "";
+        for (String s : str) {
+            if (!str1.contains(s)) str1.add(s);
+        }
+        return str1;
+    }
 
+    /**
+     * Write a method removeExtraSpaces() that takes a String as an argument,
+     * and returns a String with removed extra spaces
+     */
+    public static String removeExtraSpaces(String str) {
+        String[] s1 = str.trim().split(" ");
+        String str1 = "";
+        for (String s : s1) {
+            if (!s.isEmpty()) str1 += s + " ";
+        }
+        return str1.substring(0, str1.length() - 1);
+    }
+
+    /**
+     * Write a method add() that takes 2 int[] arrays as arguments and
+     * returns a new array with sum of given arrays elements.
+     */
+    public static int[] add(int[] a, int[] b) {
+        for (int i = 0; i < Math.min(a.length, b.length); i++) {
+            if (a.length > b.length) a[i] += b[i];
+            else b[i] += a[i];
+        }
+        return (a.length > b.length) ? a : b;
+    }
+
+    /**
+     * Write a method findClosestTo10() that takes an int[] array as an argument,
+     * and returns the closest element to 10 from given array
+     */
+    public static int findClosestTo10(int[] numbers) {
+        Arrays.sort(numbers);
+        int leftClosest = Integer.MIN_VALUE;
+        int rightClosest = Integer.MAX_VALUE;
+
+        for (int number : numbers) {
+            if (number < 10 && number > leftClosest) leftClosest = number;
+            else if (number > 10 && number < rightClosest) rightClosest = number;
+        }
+        if (10 - leftClosest <= rightClosest - 10) return leftClosest;
+        return rightClosest;
+    }
 }
