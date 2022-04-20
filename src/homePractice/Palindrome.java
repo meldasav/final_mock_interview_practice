bf28e96d298233a452d1c49b354ee406c4b10046
meldasav <gmeldag83@hotmail.com>
2022-04-20 09:56:32 -0500

    added palindrome different way

--- src/homePractice/Palindrome.java
+++ src/homePractice/Palindrome.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+        System.out.println(isPalindrome( "  "));
         System.out.println("\nTASK2\n");
 
         Scanner scan = new Scanner(System.in);
@@ -18,4 +19,21 @@ public class Palindrome {
             System.out.println("this word does not have 1 or more characters");
         }
     }
+
+
+    public static boolean isPalindrome(String str) {
+        String reversed="";
+        if (str.isEmpty()) return false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed+=(str.charAt(i));
+        }
+        return str.trim().equals(reversed.trim().toString());
+    }
+
+    public static boolean isPalindrome1(String str) {
+        if(str.isEmpty())return false;
+        return str.trim().equals(new StringBuilder(str.trim()).reverse().toString());
+    }
+
+
 }
